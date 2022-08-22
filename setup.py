b8ea6e077afc9a0da25825395e73e279c4ce2298
Sam Sweere <samsweere@gmail.com>
2022-08-22 12:24:26 +0200

    added example files

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='xmm-superres-denoise',
+    name="xmm-superres-denoise",
     packages=find_packages(),
-    version='1.0.0',
-    description='XMM-SuperRes and XMM-DeNoise training and inference code',
-    author='Sam Sweere',
-    license='',
+    version="1.0.0",
+    description="XMM-SuperRes and XMM-DeNoise training and inference code",
+    author="Sam Sweere",
+    license="",
 )
