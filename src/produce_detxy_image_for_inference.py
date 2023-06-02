7e9e465e3db0161f94bf96a212ba2747df674d86
Bojan Todorkov <bojan@todorkov.de>
2023-06-02 16:38:27 +0200

    Refactoring of inference procedure

--- src/produce_detxy_image_for_inference.py
+++ src/produce_detxy_image_for_inference.py
@@ -12,51 +12,72 @@
 # Created Oct 2022, _Ivan Valtchanov_, XMM SOC
 #
 
-import os
+from argparse import ArgumentParser
+from pathlib import Path
 
-from utils.xmmsas_tools import check_pps_dir, make_gti_pps, filter_events_gti, make_detxy_image, get_pps_nxsa
+from utils.xmmsas_tools import make_gti_pps, filter_events_gti, make_detxy_image, get_pps_nxsa
 
-# Set top_d to a folder where to download the PPS
-top_d = os.getcwd()
-if (not os.path.isdir(top_d)):
-    print ('Error: working folder {top_d} does not exist, cannot continue!')
-    raise FileNotFoundError
-#
-obsid = '0852030101' # M51 (from paper, Fig. 7 bottom)
-pps_d = f'{top_d}/{obsid}/pps'
-#
-pps_files = get_pps_nxsa(obsid,wdir=top_d,skip=True,keeptar=False,verbose=True)
-#
-proc_d = f'{top_d}/{obsid}/proc'
-if (not os.path.isdir(proc_d)):
-    os.mkdir(proc_d)
-#
-os.chdir(proc_d)
 
-pps_files = check_pps_dir(pps_dir=pps_d,verbose=True)
+def get_detxy_for_obs_id(
+        obs_id: str,
+        obs_dir: Path,
+        proc_dir: Path,
+        pps_dir: Path,
+        instrument: str,
+        max_expo: int
+):
+    proc_dir.mkdir(parents=True, exist_ok=True)
+    suffix = f"{max_expo}ks.fits" if max_expo > 0 else "full.fits"
+    pps_files = get_pps_nxsa(obs_id, w_dir=obs_dir)
+    png_name = f"{instrument}_gti_{suffix.replace('fits', 'png')}"
+    gtis = make_gti_pps(pps_files=pps_files, instrument=instrument, out_dir=proc_dir, max_expo=max_expo, plot_it=True,
+                        save_plot=png_name)
 
-expo_time = 20 # ks
-gtis = make_gti_pps(pps_d,instrument='all',out_dir=proc_d,max_expo=expo_time,plot_it=True,save_plot=f'all_gti_{expo_time}ks_figure.png')
-if (len(gtis) < 1):
-    raise RuntimeError
-# select the PN event list in the PPS
-for ifile in pps_files['evl_files']:
-    if ('PN' in ifile.upper()):
-        evl = ifile
-        break
-for ifile in gtis:
-    if ('PN' in ifile.upper()):
-        pn_gti = ifile
-        break
-#
-evl_filt = filter_events_gti(evl,pn_gti,pps_dir=pps_d,output_name=f'pn_cleaned_evl_{expo_time}ks.fits',verbose=True)
-if (evl_filt is None):
-    raise RuntimeError
-#
-e1 = 500
-e2 = 2000
-detxy = make_detxy_image(evl_filt,pps_dir=pps_d,output_name=f'pn_{e1}_{e2}_detxy_image_{expo_time}ks.fits',low_energy=e1,high_energy=e2,bin_size=80,radec_image=True,verbose=True)
-if (detxy is None):
-    raise RuntimeError
-#
-print ('Test done')
+    if len(gtis) < 1:
+        raise RuntimeError(f"No GTIs found.")
+
+    evl = None
+    pn_gti = None
+    # select the PN event list in the PPS
+    for file in pps_files["evl_files"]:
+        if 'PN' in file.stem.upper():
+            evl = file
+            break
+
+    for file in gtis:
+        if 'PN' in file.stem.upper():
+            pn_gti = file
+            break
+
+    evl_filtered = filter_events_gti(evl, pn_gti, pps_files=pps_files, verbose=True,
+                                     output_name=f'{instrument}_cleaned_evl_{suffix}', w_dir=obs_dir)
+
+    if evl_filtered is None:
+        raise RuntimeError("No filtered event lists found!")
+
+    det_xy = make_detxy_image(event_list=evl_filtered, pps_dir=pps_dir, pps_files=pps_files, low_energy=500,
+                              high_energy=2000, bin_size=80, radec_image=False, verbose=True,
+                              output_name=f'{instrument}_500_2000_detxy_image_{suffix}', w_dir=obs_dir)
+
+    return det_xy
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description='Create det_xy image for given observation')
+    parser.add_argument('obsid', type=str,
+                        help='The OBS_ID to process')
+    parser.add_argument('--wdir', type=Path, default=Path.cwd(),
+                        help='The working top folder name, must exist')
+    parser.add_argument('--expo_time', type=float, default=20,
+                        help='Will extract only this exposure time (in ks) from the event list. '
+                             'Set it to negative to use the GTI one.')
+    args = parser.parse_args()
+
+    get_detxy_for_obs_id(
+        obs_id=args.obs_id,
+        obs_dir=args.wdir / args.obs_id,
+        proc_dir=args.wdir / args.obs_id / "proc",
+        pps_dir=args.wdir / args.obs_id / "pps",
+        instrument="all",
+        max_expo=args.expo_time
+    )
