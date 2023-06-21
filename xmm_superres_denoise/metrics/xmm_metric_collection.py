4d40bb9375a2dd96cd5976e5f9b0f55bb0b9e3f8
Bojan Todorkov <bojan@todorkov.de>
2023-06-21 09:55:03 +0200

    Missing file

---
+++ xmm_superres_denoise/metrics/xmm_metric_collection.py
@@ -0,0 +1,168 @@
+from typing import List, Union
+
+import torch
+from torchmetrics import (
+    MeanAbsoluteError,
+    MeanSquaredError,
+    MetricCollection,
+    MultiScaleStructuralSimilarityIndexMeasure,
+    PeakSignalNoiseRatio,
+    StructuralSimilarityIndexMeasure,
+)
+
+from xmm_superres_denoise.metrics import (
+    FSIM,
+    GMSD,
+    MDSI,
+    VIF,
+    HaarPSI,
+    MultiScaleGMSD,
+    PoissonNLLLoss,
+)
+from xmm_superres_denoise.transforms import Normalize
+
+
+def get_metrics(
+    data_range: Union[int, float],
+    dataset_normalizer: Normalize,
+    scaling_normalizers: List[Normalize],
+    prefix: str,
+):
+    metrics = MetricCollection(
+        {
+            "psnr": PeakSignalNoiseRatio(),
+            "ssim": StructuralSimilarityIndexMeasure(
+                kernel_size=13, sigma=2.5, k2=0.05
+            ),
+            "ms_ssim": MultiScaleStructuralSimilarityIndexMeasure(
+                kernel_size=13, sigma=2.5, k2=0.05
+            ),
+            "l1": MeanAbsoluteError(),
+            "l2": MeanSquaredError(),
+            "poisson": PoissonNLLLoss(),
+        }
+    )
+    return XMMMetricCollection(
+        data_range=data_range,
+        metrics=metrics,
+        dataset_normalizer=dataset_normalizer,
+        scaling_normalizers=scaling_normalizers,
+        prefix=prefix,
+    )
+
+
+def get_ext_metrics(
+    data_range: Union[int, float],
+    dataset_normalizer: Normalize,
+    scaling_normalizers: List[Normalize],
+    prefix: str,
+):
+    metrics = MetricCollection(
+        {
+            "vif_p": VIF(),
+            "fsim": FSIM(),
+            "gmsd": GMSD(),
+            "ms_gmsd": MultiScaleGMSD(),
+            "haarpsi": HaarPSI(),
+            "msdi": MDSI(),
+        }
+    )
+    return XMMMetricCollection(
+        data_range=data_range,
+        metrics=metrics,
+        dataset_normalizer=dataset_normalizer,
+        scaling_normalizers=scaling_normalizers,
+        prefix=prefix,
+    )
+
+
+def get_in_metrics(
+    data_range: Union[int, float],
+    dataset_normalizer: Normalize,
+    scaling_normalizers: List[Normalize],
+    prefix: str,
+):
+    metrics = MetricCollection(
+        {
+            "in/psnr": PeakSignalNoiseRatio(),
+            "in/ssim": StructuralSimilarityIndexMeasure(
+                kernel_size=13, sigma=2.5, k2=0.05
+            ),
+            "in/ms_ssim": MultiScaleStructuralSimilarityIndexMeasure(
+                kernel_size=13, sigma=2.5, k2=0.05
+            ),
+            "in/l1": MeanAbsoluteError(),
+            "in/l2": MeanSquaredError(),
+            "in/poisson": PoissonNLLLoss(),
+        }
+    )
+    return XMMMetricCollection(
+        data_range=data_range,
+        metrics=metrics,
+        dataset_normalizer=dataset_normalizer,
+        scaling_normalizers=scaling_normalizers,
+        prefix=prefix,
+    )
+
+
+def get_in_ext_metrics(
+    data_range: Union[int, float],
+    dataset_normalizer: Normalize,
+    scaling_normalizers: List[Normalize],
+    prefix: str,
+):
+    metrics = MetricCollection(
+        {
+            "in/vif_p": VIF(),
+            "in/fsim": FSIM(),
+            "in/gmsd": GMSD(),
+            "in/ms_gmsd": MultiScaleGMSD(),
+            "in/haarpsi": HaarPSI(),
+            "in/msdi": MDSI(),
+        }
+    )
+    return XMMMetricCollection(
+        data_range=data_range,
+        metrics=metrics,
+        dataset_normalizer=dataset_normalizer,
+        scaling_normalizers=scaling_normalizers,
+        prefix=prefix,
+    )
+
+
+class XMMMetricCollection(MetricCollection):
+    def __init__(
+        self,
+        data_range: Union[int, float],
+        metrics: MetricCollection,
+        dataset_normalizer: Normalize,
+        scaling_normalizers: List[Normalize],
+        prefix: str,
+    ):
+        self.data_range = data_range
+        self.dataset_normalizer = dataset_normalizer
+
+        metric_list = []
+        normalizer_dict = {}
+
+        for normalizer in scaling_normalizers:
+            mode = normalizer.stretch_mode
+            normalizer_dict[mode] = normalizer
+            metric_list.append(metrics.clone(prefix=f"{mode}/"))
+
+        self.normalizer_dict = normalizer_dict
+        super(XMMMetricCollection, self).__init__(
+            metrics=metric_list, prefix=f"{prefix}/"
+        )
+
+    def update(self, preds, target) -> None:
+        preds = self.dataset_normalizer.denormalize_hr_image(preds)
+        target = self.dataset_normalizer.denormalize_hr_image(target)
+        preds = torch.clamp(preds, min=0.0, max=self.data_range)
+        target = torch.clamp(target, min=0.0, max=self.data_range)
+        for metric_name, metric in self.items(copy_state=False):
+            mode = metric_name.split("/")[1]
+            normalizer = self.normalizer_dict[mode]
+            preds_scaled = normalizer.normalize_hr_image(preds)
+            target_scaled = normalizer.normalize_hr_image(target)
+            metric.update(preds=preds_scaled, target=target_scaled)
