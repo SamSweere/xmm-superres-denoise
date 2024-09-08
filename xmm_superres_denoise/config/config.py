61c7e7624191373190c3ade00ff21e33566e4390
Bojan Todorkov <bojan@todorkov.de>
2024-09-08 21:33:12 +0200

    WIP

--- xmm_superres_denoise/config/config.py
+++ xmm_superres_denoise/config/config.py
@@ -1,10 +1,25 @@
-from pydantic import BaseModel, PositiveInt
-
+from pydantic import computed_field, BaseModel, NonNegativeFloat, NonNegativeInt, PositiveInt
 from typing import Literal
 from pathlib import Path
 
 
-class Dataset(BaseModel):
+class HrDatasetCfg(BaseModel):
+    agn: bool
+    bkg: bool
+    exp: PositiveInt
+    clamp_max: NonNegativeFloat
+    res: PositiveInt
+
+
+class LrDatasetCfg(BaseModel):
+    agn: bool | NonNegativeInt
+    bkg: bool | NonNegativeInt
+    exps: list[PositiveInt]
+    clamp_max: NonNegativeFloat
+    res: PositiveInt
+
+
+class DatasetCfg(BaseModel):
     batch_size: PositiveInt
     check_files: bool
     crop_mode: Literal["center", "random", "boresight"]
@@ -14,3 +29,17 @@ class Dataset(BaseModel):
     name: str
     scaling: Literal["linear", "sqrt", "asinh", "log"] | None
     type: Literal["sim", "real"]
+    lr: LrDatasetCfg
+    hr: HrDatasetCfg
+
+    @computed_field
+    @property
+    def res_mult(self) -> int:
+        return self.hr.res // self.lr.res
+
+
+class OptimizerCfg(BaseModel):
+    learning_rate: NonNegativeFloat
+    betas: tuple[NonNegativeFloat]
+
+class ModelCfg(BaseModel):
