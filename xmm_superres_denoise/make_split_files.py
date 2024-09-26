f3550372725082ae50d2be91994602dfd33bcc77
yveggers <yvonne.eggers@esa.int>
2024-09-26 17:03:35 +0000

    Added validation test

--- xmm_superres_denoise/make_split_files.py
+++ xmm_superres_denoise/make_split_files.py
@@ -64,11 +64,11 @@ def make_gen_split_files(data, args, subset_indices, stages):
     subset_indices (list): List of PyTorch tensors with subset indices.
     stages (list): List of strings containing the subset names.
     """
-    means, variances, maxes, quantiles, fractions_over_lr_max_all = data
+    means, variances, maxes, quantiles, fractions_over_lr_max = data
 
     # Identify indices above and below the fraction threshold
-    above_indices = torch.where(fractions_over_lr_max_all > args.fraction)[0]
-    below_indices = torch.where(fractions_over_lr_max_all <= args.fraction)[0]
+    above_indices = torch.where(fractions_over_lr_max > args.fraction)[0]
+    below_indices = torch.where(fractions_over_lr_max <= args.fraction)[0]
 
     # Define filenames based on input type
     filename_blw = f'below_{args.exp}ks_{args.pixel_res}px_{args.agns}_img.p' if args.input_type == 'sim' else f'{args.agns}_{args.pixel_res}px_{args.exp}ks_below.p'
@@ -93,6 +93,17 @@ def make_gen_split_files(data, args, subset_indices, stages):
         # Construct file paths
         below_path = base_dir + f'{stages[i]}/{filename_blw}'
         above_path = base_dir + f'{stages[i]}/{filename_abv}'
+        
+        # Print the test 
+        below_test = torch.sum(fractions_over_lr_max[below_stage_indices]>args.fraction)
+        above_test = torch.sum(fractions_over_lr_max[above_stage_indices]<=args.fraction)
+
+        print('Stage: {stage}'.format(stage = stages[i]))
+        print('Below test: {below_test} inputs with {fraction} of the image over lr_max'.format(below_test = below_test, fraction = args.fraction))
+        print('Above test: {above_test} inputs with {fraction} of the image below lr_max'.format(above_test = above_test, fraction = args.fraction))
+        print()
+
+     
 
         # Save the indices to the file
         with open(below_path, "wb") as f:
@@ -100,6 +111,7 @@ def make_gen_split_files(data, args, subset_indices, stages):
 
         with open(above_path, "wb") as f:
             pickle.dump(np.asarray(above_stage_indices), f)
+            
             
             
 def make_dis_split_files(data, args):
@@ -110,11 +122,11 @@ def make_dis_split_files(data, args):
     data (tuple): Data containing means, variances, maxes, quantiles, and fractions over lr max.
     args (): Input arguments from terminal
     """
-    means, variances, maxes, quantiles, fractions_over_lr_max_all = data
+    means, variances, maxes, quantiles, fractions_over_lr_max = data
 
     # Identify indices above and below the fraction threshold
-    above_indices = torch.where(fractions_over_lr_max_all > args.fraction)[0]
-    below_indices = torch.where(fractions_over_lr_max_all <= args.fraction)[0]
+    above_indices = torch.where(fractions_over_lr_max > args.fraction)[0]
+    below_indices = torch.where(fractions_over_lr_max <= args.fraction)[0]
 
     # Define filenames based on input type
     filename_blw = f'below_{args.exp}ks_{args.pixel_res}px_{args.agns}_img.p' if args.input_type == 'sim' else f'{args.agns}_{args.pixel_res}px_{args.exp}ks_below.p'
@@ -126,6 +138,15 @@ def make_dis_split_files(data, args):
     # Construct file paths
     below_path = base_dir + f'{filename_blw}'
     above_path = base_dir + f'{filename_abv}'
+    
+    # Print the test 
+    below_test = torch.sum(fractions_over_lr_max[below_indices]>args.fraction)
+    above_test = torch.sum(fractions_over_lr_max[above_indices]<=args.fraction)
+
+    print('Below test: {below_test} inputs with {fraction} of the image over lr_max'.format(below_test = below_test, fraction = args.fraction))
+    print('Above test: {above_test} inputs with {fraction} of the image below lr_max'.format(above_test = above_test, fraction = args.fraction))
+    print()
+
 
     # Save the indices to the file
     with open(below_path, "wb") as f:
@@ -167,7 +188,7 @@ if __name__ == "__main__":
     # Setup argparse for terminal inputs
     parser = argparse.ArgumentParser(description='Process statistics and subset indices.')
     parser.add_argument('--exp', type=int, default=20, help='Exposure value in ks (default: 20).')
-    parser.add_argument('--input_type', type=str, default='real', choices=['sim', 'real', 'sim_display', 'real_display'], help='Input type: "sim" or "real" (default: sim).')
+    parser.add_argument('--input_type', type=str, default='real_display', choices=['sim', 'real', 'sim_display', 'real_display'], help='Input type: "sim" or "real" (default: sim).')
     parser.add_argument('--pixel_res', type=int, default=256, help='Pixel resolution (default: 256).')
     parser.add_argument('--res_name', type=str, default='lr', help='Resolution name (default: lr).')
     parser.add_argument('--agns', type=str, default='no_blended_agn', help='AGN type (default: no_blended_agn).')
