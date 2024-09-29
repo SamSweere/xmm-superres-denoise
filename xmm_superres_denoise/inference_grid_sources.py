ccf2476686d20545451a55c5c59e08e94e29b583
yveggers <yvonne.eggers@esa.int>
2024-09-29 22:30:24 +0000

    Changed path names to relative paths

--- xmm_superres_denoise/inference_grid_sources.py
+++ xmm_superres_denoise/inference_grid_sources.py
@@ -6,7 +6,7 @@ from pytorch_lightning.utilities import rank_zero_warn
 import argparse
 
 
-def main(clamping_tr_nn, res, exp, base_path, run_config, checkpoint, downscales, br_diffs_scales, shift_amounts):
+def main(clamping_tr_nn, base_path, run_config, checkpoint, downscales, br_diffs_scales, shift_amounts, res = 1, exp = 20):
     
     grid_dirs = [
         f'test_grid_f_1e-13_step_10_mult_{res}_{exp}ks_p_0-4.fits.gz',
@@ -52,12 +52,10 @@ def main(clamping_tr_nn, res, exp, base_path, run_config, checkpoint, downscales
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run inference on FITS files with specified parameters.')
 
-    parser.add_argument('--clamping_tr_nn', type=str, default='original', help='Name of the employed clamping')
-    parser.add_argument('--res', type=int, default=1, help='Resolution factor of the input file.')
-    parser.add_argument('--exp', type=int, default=20, help='Exposure time of the input file in ks.')
-    parser.add_argument('--base_path', type=str, default='/home/xmmsas/mywork/cleanup_new/xmm-superres-denoise/shifted_grid_sources/results/', help='Base directory for results.')
-    parser.add_argument('--run_config', type=Path, default=Path('/home/xmmsas/mywork/cleanup_new/xmm-superres-denoise/res/baseline_config.yaml'), help='Path to run configuration YAML file.')
-    parser.add_argument('--checkpoint', type=Path, default=Path('/home/xmmsas/mywork/cleanup_new/xmm-superres-denoise/models/original_model_epoch_49_esr_gen_2024-09-23_06-41-32.ckpt'), help='Path to model checkpoint file.')
+    parser.add_argument('--clamping_tr_nn', type=str, default='deblending_original_clamp', help='Name of the training run')
+    parser.add_argument('--base_path', type=str, default='shifted_grid_sources/results/', help='Base directory for results.')
+    parser.add_argument('--run_config', type=Path, default=Path('res/baseline_config.yaml'), help='Path to run configuration YAML file.')
+    parser.add_argument('--checkpoint', type=Path, default=Path('models/deblend_original_clamp.ckpt'), help='Path to model checkpoint file.')
     parser.add_argument('--downscales', nargs='+', type=float, default=[0.25, 0.5, 0.75, 1], help='List of downscale factors.')
     parser.add_argument('--br_diffs_scales', nargs='+', type=float, default=[1], help='List of brightness difference scales.')
     parser.add_argument('--shift_amounts', nargs='+', type=int, default=[0, 1, 2, 3, 4], help='List of shift amounts.')
@@ -70,11 +68,10 @@ if __name__ == "__main__":
     )
     
     os.environ["CUDA_VISIBLE_DEVICES"] = '0,1,2'
+    print(Path.cwd())
 
     main(
         args.clamping_tr_nn, 
-        args.res, 
-        args.exp, 
         args.base_path, 
         args.run_config, 
         args.checkpoint, 
